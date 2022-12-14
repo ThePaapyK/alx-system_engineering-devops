77a02d1caa2b8772e2a15fa0e345d843d708043b
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-14 16:56:30 +0000

    Just

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,21 @@
 # changes configuration file
-file { '~/.ssh/config':
-    ensure  => 'file',
-    content => "IdentityFile ~/.ssh/school
-                PasswordAuthentication no
-               "
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
+
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+
