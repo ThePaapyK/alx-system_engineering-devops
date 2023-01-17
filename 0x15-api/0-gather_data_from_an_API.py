4169f1f078a5802b5cd62a040d301f90f000099f
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-17 00:16:09 +0000

    api actions

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """for a given employee ID, returns information \
 about his/her TODO list progress."""
-from sys import argv
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/todos/{}".format(argv[1])
-    url2 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    r = requests.get(url)
-    r2 = requests.get(url2)
+    url = "https://jsonplaceholder.typicode.com/todos"
+    url2 = "https://jsonplaceholder.typicode.com/users"
+    r = requests.get(url, params={"userId": argv[1]})
+    r2 = requests.get(url2, params={"id": argv[1]})
     c = r.json()
     d = r2.json()
     n = 0
     for item in c:
         if item['completed'] is True:
             n = n + 1
-    print("Employee {} is done with \
-           tasks({}/{}):".format(d['name'], n, len(c)))
+    print("Employee {} is done with tasks({}/{}):".format(d[0]['name'],
+                                                          n, len(c)))
     for todo in c:
-        print("\t {}".format(todo['title']))
+        if todo['completed'] is True:
+            print("\t {}".format(todo['title']))
