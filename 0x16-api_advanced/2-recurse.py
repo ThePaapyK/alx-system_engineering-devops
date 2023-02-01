54edd08cf38048962383a563b6933795a23d6148
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-02-01 01:25:09 +0000

    recurse it

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a recursive function that queries the Reddit API and returns \
+a list containing the titles of all hot articles for a given \
+subreddit. If no results are found for the given \
+subreddit, the function should return None."""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], _next=None):
+    """returns titles of all hot articles of a subreddit
+    Args:
+    subreddit (string): name of given subreddit"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    r = requests.get(url, headers={"User-Agent": "globann"},
+                     allow_redirects=False,
+                     params={'after': _next})
+    if r.status_code == 200:
+        _next = r.json()['data']['after']
+        if _next is None:
+            return hot_list
+
+        for post in r.json()['data']['children']:
+            hot_list.append(post['data']['title'])
+        return recurse(subreddit, hot_list, _next)
+    else:
+        return None
